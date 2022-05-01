8e38a4cd06d6e0e72665ceea19379124c9faca42
a-bodrova <soulwent94@gmail.com>
2022-05-01 12:28:37 +0400

    feat: add class Queue implementation

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -24,7 +24,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function removeKFromList(/* l, k */) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (!l.value) return null;
+
+  while (l.value) {
+    if (l.value === k) {
+      if (l.next) {
+        l.value = l.next;
+      } else {
+        l.value = null;
+      }
+    } else {
+      l.value = l.value.next;
+    }
+  }
+  
+  return l;
 }
 
 module.exports = {
